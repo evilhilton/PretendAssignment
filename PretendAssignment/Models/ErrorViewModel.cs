22f9a8b3c30e48b44e3af721e48c37f6a1d50649
spencerhilton <shilton1@byu.edu>
2022-02-05 15:08:19 -0700

    Initial Push
    This is the initial repository creation.

---
+++ PretendAssignment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PretendAssignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
